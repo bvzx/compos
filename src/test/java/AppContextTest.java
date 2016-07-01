98db7aea6dd79b7cb6d786fb43c02a14e1a5ee4b
wugaoda <wugaoda@caitu99.com>
2016-07-01 11:52:23 +0800

    no message

--- src/test/java/AppContextTest.java
+++ src/test/java/AppContextTest.java
@@ -1,10 +1,8 @@
 import base.SpringTest;
 import io.bvzx.service.AppContext;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import util.LogUtils;
 
 /**
  * Created by 73992 on 2016/6/30.
